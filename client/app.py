8af6ca9c85973971829560111e8fb6cf7ba4a60e
Natalie Moraes <natalie.gomes17@gmail.com>
2023-12-15 16:09:41 -0300

    added soil moisture script

--- client/app.py
+++ client/app.py
@@ -1,5 +1,7 @@
 from counterfit_connection import CounterFitConnection
 from counterfit_shims_seeed_python_dht import DHT
+from counterfit_shims_grove.adc import ADC
+from counterfit_shims_grove.grove_relay import GroveRelay
 
 import json
 import time
@@ -12,6 +14,7 @@ id = '91c4a0a0-0e35-40cb-a603-3b42611f15dd'  # gerado no guidgen
 client_name = id + 'embarcados_client'
 client_telemetry_topic_sensor1 = id + '/telemetry/sensor1'
 client_telemetry_topic_sensor2 = id + '/telemetry/sensor2'
+client_telemetry_topic_sensor3 = id + '/telemetry/sensor3'
 server_command_topic = id + '/commands'
 mqtt_client = mqtt.Client(client_name)
 mqtt_client.connect('test.mosquitto.org')
@@ -20,6 +23,13 @@ print('MQTT connected')
 
 sensor1 = DHT("11", 0)
 sensor2 = DHT("11", 2)
+sensor3 = DHT("11", 4)
+adc1 = ADC(7)
+adc2 = ADC(9)
+adc3 = ADC(11)
+relay1 = GroveRelay(6)
+relay2 = GroveRelay(8)
+relay3 = GroveRelay(10)
 
 
 def handle_command(client, userdata, message):
@@ -29,15 +39,62 @@ def handle_command(client, userdata, message):
     payload_sensor2 = json.loads(message.payload.decode())
     print('Message received for sensor 2:', payload_sensor2)
 
+    payload_sensor3 = json.loads(message.payload.decode())
+    print('Message received for sensor 3:', payload_sensor3)
+
 while True:
+    soil_moisture1 = adc1.read(0)
+    print("Soil moisture1:", soil_moisture1)
+
+    if soil_moisture1 > 450:
+        print("Soil Moisture 1 is too low, turning relay1 on.")
+        relay1.on()
+        time.sleep(5)
+        print("Turning relay1 off.")
+        relay1.off()
+    else:
+        print("Soil Moisture 1 is ok, turning relay1 off.")
+        relay1.off()
+
+    soil_moisture2 = adc2.read(0)
+    print("Soil moisture2:", soil_moisture2)
+
+    if soil_moisture2 > 450:
+        print("Soil Moisture 2 is too low, turning relay2 on.")
+        relay2.on()
+        time.sleep(5)
+        print("Turning relay2 off.")
+        relay2.off()
+    else:
+        print("Soil Moisture 2 is ok, turning relay2 off.")
+        relay2.off()
+
+    soil_moisture3 = adc3.read(0)
+    print("Soil moisture3:", soil_moisture3)
+
+    if soil_moisture3 > 450:
+        print("Soil Moisture 3 is too low, turning relay3 on.")
+        relay3.on()
+        time.sleep(5)
+        print("Turning relay3 off.")
+        relay3.off()
+    else:
+        print("Soil Moisture 3 is ok, turning relay3 off.")
+        relay1.off()
+
     _, temperature_sensor1 = sensor1.read()
-    telemetry_sensor1 = json.dumps({'temperature_sensor1': temperature_sensor1})
+    telemetry_sensor1 = json.dumps({'temperature_sensor1': temperature_sensor1, 'umidade_sensor1': soil_moisture1})
     print(f'temperature_sensor1 {temperature_sensor1}°C')
     mqtt_client.publish(client_telemetry_topic_sensor1, telemetry_sensor1)
 
     _, temperature_sensor2 = sensor2.read()
-    telemetry_sensor2 = json.dumps({'temperature_sensor2': temperature_sensor2})
+    telemetry_sensor2 = json.dumps({'temperature_sensor2': temperature_sensor2, 'umidade_sensor2': soil_moisture2})
     print(f'temperature_sensor2 {temperature_sensor2}°C')
     mqtt_client.publish(client_telemetry_topic_sensor2, telemetry_sensor2)
 
-    time.sleep(10)
+    _, temperature_sensor3 = sensor3.read()
+    telemetry_sensor3 = json.dumps({'temperature_sensor3': temperature_sensor3, 'umidade_sensor3': soil_moisture2})
+    print(f'temperature_sensor3 {temperature_sensor3}°C')
+    mqtt_client.publish(client_telemetry_topic_sensor2, telemetry_sensor2)
+
+    time.sleep(30)
--- server/app.py
+++ server/app.py
@@ -7,6 +7,7 @@ from datetime import datetime
 id = '91c4a0a0-0e35-40cb-a603-3b42611f15dd'  # gerado no guidgen
 client_telemetry_topic_sensor1 = id + '/telemetry/sensor1'
 client_telemetry_topic_sensor2 = id + '/telemetry/sensor2'
+client_telemetry_topic_sensor3 = id + '/telemetry/sensor3'
 server_command_topic = id + '/commands'
 client_name = id + 'embarcados_server'
 mqtt_client = mqtt.Client(client_name)
@@ -19,17 +20,20 @@ tmax2 = float('-inf')
 tmin2 = float('inf')
 
 def handle_telemetry(client, userdata, message):
-    global tmax1, tmin1, tmax2, tmin2
+    global tmax1, tmedia1, tmin1, tmax2, tmin2, tmedia2, tmax3, tmedia3, tmin3 
+    global irrigar, tempo_medicao
+
+    tempo_medicao = 30 # intervalo das medicoes
 
     endpoint_url = 'http://localhost:5088/api/temperatura'
+    endpoint_irrigacao = 'http://localhost:5088/api/Estado_Irrigacao/'
     current_datetime = datetime.now()
-    current_date_str = current_datetime.strftime("%d-%m-%Y")
-    current_time_str = current_datetime.strftime("%H:%M:%S")
     current_time = datetime.strptime(current_time_str, "%H:%M:%S").time()
     start_time = datetime.strptime("08:00:00", "%H:%M:%S").time()
     end_time = datetime.strptime("17:05:00", "%H:%M:%S").time()
     payload = json.loads(message.payload.decode())
 
+    #Enviar informações de temperatura sensor 1
     if 'temperature_sensor1' in payload:
         temperature_sensor1 = payload['temperature_sensor1']
         print('Message received from sensor 1:', temperature_sensor1)
@@ -41,13 +45,16 @@ def handle_telemetry(client, userdata, message):
             tmax1 = temperature_sensor1
             print(f'temperature_sensor1: {temperature_sensor1}, tmax1: {tmax1}')
 
+        tmedia1 = (tmax1 + tmin1)/2
+
         command_sensor1 = {
             "sensorId": 1,
             "tmax": tmax1,
             "tmin": tmin1,
-            "data": current_date_str,
-            "hora": current_time_str
+            "tmedia": tmedia1,
+            "data": current_datetime
         }
+
         print('Sending command for sensor 1:', command_sensor1)
         
         command_json_sensor1 = json.dumps(command_sensor1)
@@ -63,6 +70,34 @@ def handle_telemetry(client, userdata, message):
         else:
             print("Waiting for telemetry transmission window.")
 
+    #Enviar informações de irrigação sensor 1
+    if 'umidade_sensor1' in payload:
+        umidade_sensor1 = payload['umidade_sensor1']
+        print('Message received from sensor 1:', umidade_sensor1)
+
+        if umidade_sensor1 > 450:
+            irrigar = True
+        else:
+            irrigar = False
+
+        command_irrigar_sensor1 = {
+            "irrigar" : irrigar
+        }
+
+        command_json_irrigar_sensor1 = json.dumps(command_irrigar_sensor1)
+
+        if start_time <= current_time <= end_time:
+            response_irrigar_sensor1 = requests.put(endpoint_irrigacao+'1', data=command_json_irrigar_sensor1, headers={'Content-Type': 'application/json'})
+        
+            if response_irrigar_sensor1.status_code == 201:
+                print("Command for sensor 1 successfully sent!")
+            else:
+                print("Error sending command for sensor 1:", response_irrigar_sensor1.status_code)
+                print("Response content:", response_irrigar_sensor1.content)
+        else:
+            print("Waiting for telemetry transmission window.")
+
+    #Enviar informações de temperatura sensor 2
     if 'temperature_sensor2' in payload:
         temperature_sensor2 = payload['temperature_sensor2']
         print('Message received from sensor 2:', temperature_sensor2)
@@ -71,13 +106,15 @@ def handle_telemetry(client, userdata, message):
             tmin2 = temperature_sensor2
         if temperature_sensor2 > tmax2:
             tmax2 = temperature_sensor2
+        
+        tmedia2 = (tmax2 + tmin2)/2
             
         command_sensor2 = { 
             "sensorId": 2,
             "tmax": tmax2,
             "tmin": tmin2,
-            "data": current_date_str,
-            "hora": current_time_str
+            "tmedia" : tmedia2,
+            "data": current_datetime
         }
         print('Sending command for sensor 2:', command_sensor2)
         
@@ -93,10 +130,102 @@ def handle_telemetry(client, userdata, message):
                 print("Response content:", response_sensor2.content)
         else:
             print("Waiting for telemetry transmission window.")
+    
+    #Enviar informações de irrigação sensor 2
+    if 'umidade_sensor2' in payload:
+        umidade_sensor2 = payload['umidade_sensor2']
+        print('Message received from sensor 2:', umidade_sensor2)
+
+        if umidade_sensor2 > 450:
+            irrigar = True
+        else:
+            irrigar = False
+
+        command_irrigar_sensor2 = {
+            "irrigar" : irrigar
+        }
+
+        command_json_irrigar_sensor2 = json.dumps(command_irrigar_sensor2)
+
+        if start_time <= current_time <= end_time:
+            response_irrigar_sensor2 = requests.put(endpoint_irrigacao+'2', data=command_json_irrigar_sensor2, headers={'Content-Type': 'application/json'})
+        
+            if response_irrigar_sensor2.status_code == 201:
+                print("Command for sensor 1 successfully sent!")
+            else:
+                print("Error sending command for sensor 1:", response_irrigar_sensor2.status_code)
+                print("Response content:", response_irrigar_sensor2.content)
+        else:
+            print("Waiting for telemetry transmission window.")
+
+    #Enviar informações de temperatura sensor 3
+    if 'temperature_sensor3' in payload:
+        temperature_sensor3 = payload['temperature_sensor3']
+        print('Message received from sensor 3:', temperature_sensor3)
+        
+        if temperature_sensor3 < tmin3:
+            tmin3 = temperature_sensor3
+            print(f'temperature_sensor3: {temperature_sensor3}, tmax1: {tmax3}')
+        if temperature_sensor3 > tmax3:
+            tmax3 = temperature_sensor3
+            print(f'temperature_sensor3: {temperature_sensor3}, tmax3: {tmax3}')
+
+        tmedia3 = (tmax3 + tmin3)/2
+
+        command_sensor3 = {
+            "sensorId": 3,
+            "tmax": tmax3,
+            "tmin": tmin3,
+            "tmedia": tmedia3,
+            "data": current_datetime
+        }
+
+        print('Sending command for sensor 3:', command_sensor3)
+        
+        command_json_sensor3 = json.dumps(command_sensor3)
+
+        if start_time <= current_time <= end_time:
+            response_sensor3 = requests.post(endpoint_url, data=command_json_sensor3, headers={'Content-Type': 'application/json'})
+        
+            if response_sensor3.status_code == 201:
+                print("Command for sensor 3 successfully sent!")
+            else:
+                print("Error sending command for sensor 3:", response_sensor3.status_code)
+                print("Response content:", response_sensor3.content)
+        else:
+            print("Waiting for telemetry transmission window.")
+
+    #Enviar informações de irrigação sensor 3
+    if 'umidade_sensor3' in payload:
+        umidade_sensor3 = payload['umidade_sensor3']
+        print('Message received from sensor 3:', umidade_sensor3)
+
+        if umidade_sensor3 > 450:
+            irrigar = True
+        else:
+            irrigar = False
+
+        command_irrigar_sensor3 = {
+            "irrigar" : irrigar
+        }
+
+        command_json_irrigar_sensor3 = json.dumps(command_irrigar_sensor3)
+
+        if start_time <= current_time <= end_time:
+            response_irrigar_sensor3 = requests.put(endpoint_irrigacao+'3', data=command_json_irrigar_sensor3, headers={'Content-Type': 'application/json'})
+        
+            if response_irrigar_sensor3.status_code == 201:
+                print("Command for sensor 1 successfully sent!")
+            else:
+                print("Error sending command for sensor 1:", response_irrigar_sensor3.status_code)
+                print("Response content:", response_irrigar_sensor3.content)
+        else:
+            print("Waiting for telemetry transmission window.")
 
 while True:
     mqtt_client.subscribe(client_telemetry_topic_sensor1)
     mqtt_client.subscribe(client_telemetry_topic_sensor2)
+    mqtt_client.subscribe(client_telemetry_topic_sensor3)
     mqtt_client.on_message = handle_telemetry
 
-    time.sleep(10)
+    time.sleep(tempo_medicao)
